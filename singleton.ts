16433fd8d297ca58b55fabd7815c486bfe5f9ed0
funnythingz <hiroki@hackleberry.me>
2013-09-15 13:57:36 +0900

    added singleton class

---
+++ singleton.ts
@@ -0,0 +1,31 @@
+module MODULE {
+
+    export class SingletonClass {
+
+        private message: string;
+
+        private static _instance:SingletonClass = null;
+
+        constructor() {
+            if(SingletonClass._instance) {
+                throw console.log('Error: Instantiation failed');
+            }
+            SingletonClass._instance = this;
+        }
+
+        static getInstance(): SingletonClass {
+            if(SingletonClass._instance === null) {
+                SingletonClass._instance = new SingletonClass();
+            }
+            return SingletonClass._instance;
+        }
+
+        getMessage(): string {
+            return this.message;
+        }
+
+    }
+
+    export var Singleton = SingletonClass.getInstance();
+
+}
